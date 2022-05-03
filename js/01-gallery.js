460d0e1810652627ebc6267336d836195c0af618
AndreyMachula <amcz2020@gmail.com>
2022-05-03 17:36:56 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,66 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const bodyEl = document.querySelector('body');
+let modalImage;
+
+
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+const createGalleryMarkup = galleryItems
+  .map(({ original, preview, description }) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image"src="${preview}" data-source="${original}" alt= "${description}"/>
+        </a>
+    </div>`)
+  .join("");
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+
+// Реализация делегирования на div.gallery и получение url большого изображения.
+
+const onGalleryClick = e => {
+  // console.log(e.target);
+
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return
+  };
+
+  onOpenModal(e.target.dataset.source);
+};
+
+galleryEl.addEventListener('click', onGalleryClick);
+
+
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox. 
+
+const onCreateModal = img => basicLightbox.create(`<img src="${img}" width="1280" alt="${img}">`);
+
+
+// Открытие модального окна по клику на элементе галереи.
+
+const onOpenModal = img => {
+  modalImage = onCreateModal(img);
+  modalImage.show();
+  // console.log("Open modal");
+  
+  document.addEventListener("keyup", onKeyPress);
+};
+
+
+// Добавление закрытия модального окна по нажатию клавиши `Escape`.
+
+const onKeyPress = e => {
+  if (e.code === "Escape") modalImage.close();
+  // console.log("Close modal with escape");
+
+  document.removeEventListener("keyup", onKeyPress);
+};
+
+// console.log(galleryItems);
